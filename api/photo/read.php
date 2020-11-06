f158d31fe50f0ad6ef9f48f4c1c1c7712a2354be
Boris Melikian <boris@lariushin.org>
2020-11-06 23:52:19 +0200

    Fix include paths.

--- api/photo/read.php
+++ api/photo/read.php
@@ -2,8 +2,8 @@
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     
-    include_once '../config/database.php';
-    include_once '../class/photos.php';
+    include_once '../../config/database.php';
+    include_once '../../class/photos.php';
 
     $database = new Database();
     $db = $database->getConnection();
@@ -14,7 +14,7 @@
     $itemCount = $stmt->rowCount();
 
 
-    echo json_encode($itemCount);
+    // echo json_encode($itemCount);
 
     if($itemCount > 0){
         
